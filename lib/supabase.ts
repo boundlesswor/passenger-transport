4e8328d7f863924bfd04acb360704b0d4cd45094
boundlesswor <workboundless@gmail.com>
2025-08-25 10:47:31 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://tjaqyqghwtekdmdqesep.supabase.co"
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYXF5cWdod3Rla2RtZHFlc2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNzMwMjEsImV4cCI6MjA3MTY0OTAyMX0.dzsNcEApLUDD98u3DjB5Ki9_Om2FM0ejabkX1EeBDFc"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
